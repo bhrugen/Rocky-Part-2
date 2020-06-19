4c330992952afdb1dd14c9662f3ecacf5c570b4b
Ben Spark <bhrugen1990@gmail.com>
2020-06-19 15:27:32 -0500

    Section 3 - Repository Interface Part 1

---
+++ Rocky_DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky_DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+         T Find(int id);
+
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null,
+            bool isTracking = true
+            );
+    }
+}
