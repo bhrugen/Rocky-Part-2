7024560cab8be9f2e6544cdbd19d82f0e547cb6b
Ben Spark <bhrugen1990@gmail.com>
2020-06-19 15:52:47 -0500

    Section 3 - Category Repository

---
+++ Rocky_DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using Rocky_DataAccess.Repository.IRepository;
+using Rocky_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky_DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db): base(db)
+        {
+            _db = db;
+        }
+        public void Update(Category obj)
+        {
+            var objFromDb = base.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = obj.Name;
+                objFromDb.DisplayOrder = obj.DisplayOrder;
+            }
+        }
+    }
+}
---
+++ Rocky_DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Rocky_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky_DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+    }
+}
