22a53f2027033a27872b9b4b8a9a613c544120e2
Ben Spark <bhrugen1990@gmail.com>
2020-06-20 13:35:43 -0500

    Section 4 - Inquiry Controller and View Model

---
+++ Rocky/Controllers/InquiryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Rocky_DataAccess;
+using Rocky_DataAccess.Repository.IRepository;
+using Rocky_Models;
+using Rocky_Utility;
+
+namespace Rocky.Controllers
+{
+    
+    public class InquiryController : Controller
+    {
+        private readonly IInquiryHeaderRepository _inqHRepo;
+        private readonly IInquiryDetailRepository _inqDRepo;
+
+        public InquiryController(IInquiryDetailRepository inqDRepo,
+            IInquiryHeaderRepository inqHRepo)
+        {
+            _inqDRepo = inqDRepo;
+            _inqHRepo = inqHRepo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Rocky_Models/ViewModels/InquiryVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky_Models.ViewModels
+{
+    public class InquiryVM
+    {
+        public InquiryHeader InquiryHeader { get; set; }
+        public IEnumerable<InquiryDetail> InquiryDetail { get; set; }
+    }
+}
