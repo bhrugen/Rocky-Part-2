58ea6c69492ac74c405f839e65f596a946d7be98
Ben Spark <bhrugen1990@gmail.com>
2020-06-20 11:31:27 -0500

    Section 4 - Inquiry Header Model

---
+++ Rocky_Models/InquiryHeader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky_Models
+{
+    public class InquiryHeader
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public DateTime InquiryDate { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
