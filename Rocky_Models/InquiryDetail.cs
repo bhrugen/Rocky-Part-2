a6d5cf093ab835f7b6a5413087c3921899de1dba
Ben Spark <bhrugen1990@gmail.com>
2020-06-20 11:35:46 -0500

    Section 4 - Inquiry Details Model

--- Rocky_Models/InquiryDetail.cs
+++ Rocky_Models/InquiryDetail.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Rocky_Models
 {
-    class InquiryDetail
+    public class InquiryDetail
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string InquiryHeaderId { get; set; }
+        [ForeignKey("InquiryHeaderId")]
+        public InquiryHeader InquiryHeader { get; set; }
+
+        [Required]
+        public string ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+
+
     }
 }
