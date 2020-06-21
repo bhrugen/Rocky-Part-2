acc831ca45e591376dc97be1ea0caa0cbd00f184
Ben Spark <bhrugen1990@gmail.com>
2020-06-21 12:42:38 -0500

    Section 6 - Shopping Cart Modifications

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -53,7 +53,15 @@ namespace Rocky.Controllers
             }
 
             List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
-            IEnumerable<Product> prodList = _prodRepo.GetAll(u => prodInCart.Contains(u.Id));
+            IEnumerable<Product> prodListTemp = _prodRepo.GetAll(u => prodInCart.Contains(u.Id));
+            IList<Product> prodList = new List<Product>();
+           
+            foreach (var cartObj in shoppingCartList)
+            {
+                Product prodTemp = prodListTemp.FirstOrDefault(u => u.Id == cartObj.ProductId);
+                prodTemp.TempSqFt = cartObj.SqFt;
+                prodList.Add(prodTemp);
+            }
 
             return View(prodList);
         }
