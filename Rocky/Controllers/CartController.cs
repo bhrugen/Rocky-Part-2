b13e9335c6fa74945e5f732ed041f7a913ed4d0a
Ben Spark <bhrugen1990@gmail.com>
2020-06-25 17:25:44 -0500

    Section 6 - Update Cart Functionality

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -193,15 +193,17 @@ namespace Rocky.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult UpdateCart()
+        public IActionResult UpdateCart(IEnumerable<Product> ProdList)
         {
             List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
-            foreach(Product prod in ProductUserVM.ProductList)
+            foreach(Product prod in ProdList)
             {
                 shoppingCartList.Add(new ShoppingCart { ProductId = prod.Id, SqFt = prod.TempSqFt });
             }
+
             HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
             return RedirectToAction(nameof(Index));
         }
     }
 }
+
