599e3db68caccdbc79df30930b08add89da1eb55
Ben Spark <bhrugen1990@gmail.com>
2020-06-26 13:14:28 -0500

    Section 6 - Validations

--- Rocky_Models/ApplicationUser.cs
+++ Rocky_Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Rocky_Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
         public string FullName { get; set; }
 
         [NotMapped]
