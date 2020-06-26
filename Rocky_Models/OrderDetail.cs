1497e6962633f36f8333dcbfe5fbda40e5b8593b
Ben Spark <bhrugen1990@gmail.com>
2020-06-26 13:24:17 -0500

    Section 7 - Order Header and Order Details Model

---
+++ Rocky_Models/OrderDetail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky_Models
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int OrderHeaderId { get; set; }
+        [ForeignKey("OrderHeaderId")]
+        public OrderHeader OrderHeader { get; set; }
+
+
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+
+        public int Sqft { get; set; }
+        public double PricePerSqFt { get; set; }
+    }
+}
---
+++ Rocky_Models/OrderHeader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky_Models
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string CreatedByUserId { get; set; }
+        [ForeignKey("CreatedByUserId")]
+        public ApplicationUser CreatedBy { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+        
+        public DateTime ShippingDate { get; set; }
+        [Required]
+        public double FinalOrderTotal { get; set; }
+        public string OrderStatus { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string TransactionId { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string PostalCode { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        public string Email { get; set; }
+    }
+}
