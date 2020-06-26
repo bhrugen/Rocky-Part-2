7e4ceff22e71a75f031265e0218183deb047dfe7
Ben Spark <bhrugen1990@gmail.com>
2020-06-26 13:00:45 -0500

    Section 6 - More fields on Order Summary

--- Rocky_Models/ApplicationUser.cs
+++ Rocky_Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,5 +10,14 @@ namespace Rocky_Models
     public class ApplicationUser : IdentityUser
     {
         public string FullName { get; set; }
+
+        [NotMapped]
+        public string StreetAddress { get; set; }
+        [NotMapped]
+        public string City { get; set; }
+        [NotMapped]
+        public string State { get; set; }
+        [NotMapped]
+        public string PostalCode { get; set; }
     }
 }
