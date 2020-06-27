7fa429e8e9e45ba55fbab8f11709764d6fb9b8a1
Ben Spark <bhrugen1990@gmail.com>
2020-06-27 12:19:45 -0500

    Section 9 - Order List View Model

---
+++ Rocky/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Rocky_DataAccess.Repository.IRepository;
+using Rocky_Utility.BrainTree;
+
+namespace Rocky.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderHeaderRepository _orderHRepo;
+        private readonly IOrderDetailRepository _orderDRepo;
+        private readonly IBrainTreeGate _brain;
+       
+        public OrderController(
+        IOrderHeaderRepository orderHRepo, IOrderDetailRepository orderDRepo, IBrainTreeGate brain)
+        {
+            _brain = brain;
+            _orderDRepo = orderDRepo;
+            _orderHRepo = orderHRepo;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Rocky_Models/ViewModels/OrderListVM.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky_Models.ViewModels
+{
+   public class OrderListVM
+    {
+        public IEnumerable<OrderHeader> OrderHList { get; set; }
+        public IEnumerable<SelectListItem> StatusList { get; set; }
+        public string Status { get; set; }
+    }
+}
