9da64546a2156498b7733c0c85f62d6da026c98a
Ben Spark <bhrugen1990@gmail.com>
2020-06-27 00:11:27 -0500

    Section 7 - BrainTree Setup Part 1

---
+++ Rocky_Utility/BrainTree/BrainTreeSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky_Utility.BrainTree
+{
+    public class BrainTreeSettings
+    {
+        public string Environment { get; set; }
+        public string MerchantId { get; set; }
+        public string PublicKey { get; set; }
+        public string PrivateKey { get; set; }
+    }
+}
---
+++ Rocky_Utility/BrainTree/IBrainTreeGate.cs
@@ -0,0 +1,16 @@
+﻿using Braintree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky_Utility.BrainTree
+{
+    public interface IBrainTreeGate
+    {
+        IBraintreeGateway CreateGateway();
+
+        IBraintreeGateway GetGateway();
+    }
+}
