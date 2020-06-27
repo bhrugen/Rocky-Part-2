32f1dd81361753ae8aca6f03a79e3a225aa07b27
Ben Spark <bhrugen1990@gmail.com>
2020-06-27 00:24:39 -0500

    Section 7 - BrainTree Setup Part 2

--- Rocky/Startup.cs
+++ Rocky/Startup.cs
@@ -16,6 +16,7 @@ using Rocky_DataAccess;
 using Rocky_DataAccess.Repository;
 using Rocky_DataAccess.Repository.IRepository;
 using Rocky_Utility;
+using Rocky_Utility.BrainTree;
 
 namespace Rocky
 {
@@ -48,6 +49,8 @@ namespace Rocky
                 Options.Cookie.HttpOnly = true;
                 Options.Cookie.IsEssential = true;
             });
+            services.Configure<BrainTreeSettings>(Configuration.GetSection("BrainTree"));
+            services.AddSingleton<IBrainTreeGate, BrainTreeGate>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IApplicationTypeRepository, ApplicationTypeRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
---
+++ Rocky_Utility/BrainTree/BrainTreeGate.cs
@@ -0,0 +1,34 @@
+﻿using Braintree;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky_Utility.BrainTree
+{
+    public class BrainTreeGate : IBrainTreeGate
+    {
+        public BrainTreeSettings _options { get; set; }
+        private IBraintreeGateway brainTreeGateWay { get; set; }
+        public BrainTreeGate(IOptions<BrainTreeSettings> options)
+        {
+            _options = options.Value;
+        }
+
+        public IBraintreeGateway CreateGateway()
+        {
+            return new BraintreeGateway(_options.Environment, _options.MerchantId, _options.PublicKey, _options.PrivateKey);
+        }
+
+        public IBraintreeGateway GetGateway()
+        {
+            if (brainTreeGateWay == null)
+            {
+                brainTreeGateWay = CreateGateway();
+            }
+            return brainTreeGateWay;
+        }
+    }
+}
