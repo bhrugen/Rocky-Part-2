ce032c12632f03335dee2731e1df702285c710fe
Ben Spark <bhrugen1990@gmail.com>
2020-06-28 15:05:18 -0500

    Section 9 - Refund Transaction

--- Rocky/Controllers/OrderController.cs
+++ Rocky/Controllers/OrderController.cs
@@ -92,11 +92,26 @@ namespace Rocky.Controllers
             _orderHRepo.Save();
             return RedirectToAction(nameof(Index));
         }
+
         [HttpPost]
         public IActionResult CancelOrder()
         {
             OrderHeader orderHeader = _orderHRepo.FirstOrDefault(u => u.Id == OrderVM.OrderHeader.Id);
-            orderHeader.OrderStatus = WC.StatusInProcess;
+
+            var gateway = _brain.GetGateway();
+            Transaction transaction = gateway.Transaction.Find(orderHeader.TransactionId);
+
+           if(transaction.Status == TransactionStatus.AUTHORIZED || transaction.Status == TransactionStatus.SUBMITTED_FOR_SETTLEMENT)
+            {
+                //no refund
+                Result<Transaction> resultvoid = gateway.Transaction.Void(orderHeader.TransactionId);
+            }
+            else
+            {
+                //refund
+                Result<Transaction> resultRefund = gateway.Transaction.Refund(orderHeader.TransactionId);
+            }
+            orderHeader.OrderStatus = WC.StatusRefunded;
             _orderHRepo.Save();
             return RedirectToAction(nameof(Index));
         }
