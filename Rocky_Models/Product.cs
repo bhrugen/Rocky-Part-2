13ebc9bea7e75407858c752bfde79eb878571d7d
Ben Spark <bhrugen1990@gmail.com>
2020-06-28 15:31:53 -0500

    Section 9 - Validation Fixes

--- Rocky_Models/Product.cs
+++ Rocky_Models/Product.cs
@@ -37,7 +37,7 @@ namespace Rocky_Models
         public virtual ApplicationType ApplicationType { get; set; }
 
         [NotMapped]
-        [Range(1,10000)]
+        [Range(1,10000,ErrorMessage ="Sqft must be greater than 0.")]
         public int TempSqFt { get; set; }
 
     }
