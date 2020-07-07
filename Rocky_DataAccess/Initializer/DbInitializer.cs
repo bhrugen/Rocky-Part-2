01c935379c3db7312bf3d960600c7c15b3ab1b09
Ben Spark <bhrugen1990@gmail.com>
2020-07-06 20:20:34 -0500

    Section 10 - Seed Database Part 1

---
+++ Rocky_DataAccess/Initializer/DbInitializer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky_DataAccess.Initializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DbInitializer(ApplicationDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Rocky_DataAccess/Initializer/IDbInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky_DataAccess.Initializer
+{
+    public interface IDbInitializer
+    {
+        void Initialize();
+    }
+}
